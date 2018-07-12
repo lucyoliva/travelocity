be0346fefccc2648ceeabe9b2c916b6350e1fd62
lucy <lucy@10.0.1.26>
2018-07-12 01:40:06 -0400

    Test

---
+++ Travelocity/src/login.java
@@ -0,0 +1,29 @@
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+//import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+public class login {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("hello");
+	WebDriver driver;
+	System.setProperty("webdriver.chrome.driver","C:\\Users\\lucy\\Downloads\\chromedriver_win32\\chromedriver.exe");
+
+	driver=new ChromeDriver();
+	driver.get("https://www.facebook.com/");
+	driver.findElement(By.name("email")).sendKeys("minnujose86@yahoo.co.in");
+	driver.findElement(By.name("pass")).sendKeys("june15198");
+	driver.findElement(By.name("pass")).sendKeys(Keys.ENTER);
+	
+
+	WebElement element;
+
+
+	}
+
+}
